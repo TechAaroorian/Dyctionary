cf9302902349ffc7c6892086623d49eee2e07023
janarthanan <janarthanan@onedatasoftware.com>
2017-12-27 16:25:09 +0530

    Deleted folders

--- Dy_SpeechReg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd SpeechRecognition-Julius
-padsp julius -C Dy_SpeechReg.jconf | python3 Dy_SpeechReg.py
--- Dyctionary_Start.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main()
-{
-	system("python3 Dyctionary_Start.py");
-}
